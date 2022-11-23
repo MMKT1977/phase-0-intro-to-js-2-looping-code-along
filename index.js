2a305ca927742154e42d05eb756093a797f5bc40
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-23 13:24:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+const gifts = ["Aki", "Ollie", "Guadalupe"];
+
+/*function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}*/
+//wrapGifts(gifts);
+
+function writeCards(gifts){
+
+  const birthdaymessage= [];
+  for (let i = 0; i < gifts.length; i++) {
+
+    birthdaymessage.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+   
+    debugger;
+  }
+ return birthdaymessage;
+}
+writeCards();
+
+function countDown(){
+  let n =10;
+  while (n>=0){
+    console.log(n);
+    n--;
+    debugger;
+  }
+  return n;
+}
+countDown();
+console.log (n);
